763412d14fd8c85c7eaad201d466a8d250fa1b7b
NiklasBuedel <buedel.niklas@stud.hs-fresenius.de>
2022-03-30 13:56:25 +0200

    Version 30.03.22

---
+++ GameDevelopment/Assets/scripts/BulletScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    public float BulletSpeed = 5f;
+    public Rigidbody2D rb2d;
+    private Vector2 screenBounds;
+    asteroidController asteroid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = this.GetComponent<Rigidbody2D>();
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb2d.velocity = new Vector2(0, BulletSpeed);
+
+        //Asteroid wird zerstört wenn er außerhalb des Bildschirms ist
+        if (transform.position.y > screenBounds.y * 2)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        //Spiel wird gestoppt bei Kollision mit Asteroid
+        asteroidController asteroid = collider.GetComponent<asteroidController>();
+        //collision.gameObject.tag == "Spawnable";
+        if (asteroid != null)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+
+
+}
